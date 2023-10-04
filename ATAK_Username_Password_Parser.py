4f2de1f74232c2b1ea50f6af7026ac6f248068f6
Jeremy Krohnfeldt <jeremykrohnfeldt@gmail.com>
2023-10-04 15:22:56 -0500

    updated to have output file

--- ATAK_Username_Password_Parser.py
+++ ATAK_Username_Password_Parser.py
@@ -16,6 +16,10 @@ for entry in data:
     usernames.append(entry["username"])
     passwords.append(entry["password"])
 
-# Now, you have the usernames and passwords in separate lists
-print("Usernames:", usernames)
-print("Passwords:", passwords)
+# Create and open an output file in write mode
+with open("output.txt", "w") as output_file:
+    # Write the usernames and passwords to the file
+    for username, password in zip(usernames, passwords):
+        output_file.write(f"Username: {username}, Password: {password}\n")
+
+print("Usernames and passwords have been exported to 'output.txt'")
